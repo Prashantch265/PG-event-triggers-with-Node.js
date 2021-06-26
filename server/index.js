37713ad8a399e31287b2a9464e06befaca461699
Prashant Chaudhary <Prashantchy265@gmail.com>
2021-06-26 22:08:24 +0545

    pg event listener complete

--- server/index.js
+++ server/index.js
@@ -9,3 +9,13 @@ client
   .connect()
   .then(() => console.log("DB connected"))
   .catch((err) => console.log(err));
+
+client
+  .query("listen new_event")
+  .then((res) => console.log(res.command))
+  .catch((e) => console.error(e.stack));
+
+client.on("notification", async (data) => {
+  const payload = JSON.parse(data.payload);
+  console.log(payload);
+});
--- server/lib/pg.js
+++ server/lib/pg.js
@@ -1,10 +1,10 @@
 const { Client } = require("pg");
 
 const client = new Client({
-  user: postgres || process.env.DBUSER,
-  host: localhost || process.env.DBHOST,
+  user: 'postgres' || process.env.DBUSER,
+  host: 'localhost' || process.env.DBHOST,
   database: process.env.DB,
-  password: postgres || process.env.DBPWD,
+  password: 'postgres' || process.env.DBPWD,
   port: 5432 || process.env.DBPORT,
 });
 
